444a4dffe13e8d9c3d81868e7bb68dffae173f5b
toan-dp <57466723+toan-dp@users.noreply.github.com>
2020-09-29 13:27:59 +0700

    lab1_test

---
+++ My First Web UI Project/Test Suites/Lab.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Lab</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0daf9876-1cbd-41db-82ae-60b6cd38af71</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcea3151-712d-41c7-bc18-1c77a87a9130</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>14fc741c-79cc-4e3b-b90b-62f39bb1f3fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3e986128-4f42-408f-a2df-9fee75c2ff2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>81d97c29-0592-47c9-997a-34fcb1723622</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>94949fc2-1bd7-4c35-ac33-79673f0072ff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
